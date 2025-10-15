67b5fb2be0464f47b585d8bac268dc4be1884a31
Suman9054 <sumanhazra368@gmail.com>
2025-10-15 20:07:28 +0530

    done some work

--- app/src/routes/api.mesagestore.$workspaceId.$Id.ts
+++ app/src/routes/api.mesagestore.$workspaceId.$Id.ts
@@ -1,30 +1,50 @@
-import { messageSchema, ServerMessage, User } from '@/types/types'
+import { messageSchema } from '@/types/types'
 import { createFileRoute } from '@tanstack/react-router'
+import { createClient } from "redis";
 
 
-const mesagestore= new Map<User,ServerMessage>()
+
+
+
 
 export const Route = createFileRoute('/api/mesagestore/$workspaceId/$Id')({
   server: {
     handlers: {
-      GET: () => {
-        const {workspaceId, Id} = Route.useParams();
-        console.log(workspaceId, Id);
+      GET: async ({params}) => {
+        try {
+        const {workspaceId, Id} = params;
+        const client = await createClient()
+        .on("error", (err) => console.log("Redis Client Error", err))
+        .connect();
+       
 
-        const usermessages = mesagestore.get({workspaceId, userId: Id});
+        const usermessages = await client.get(`lastMessage:${workspaceId}:${Id}`);
+      
+        await client.quit();
 
-        return Response.json(usermessages ?? []);
+        return Response.json(usermessages ? JSON.parse(usermessages) : null);
+        } catch (error) {
+          return Response.json({error: 'Internal Server Error',err:error}, {status: 500});
+        }
       },
-      POST: async ({ request }) => {
-       const {workspaceId, Id} = Route.useParams();
+      POST: async ({ request,params }) => {
+       try {
+        const client =  await createClient().connect();
+        client.on("error", (err) => console.log("Redis Client Error", err));
+        const {workspaceId, Id} = params ;
         const body = await request.json();
         const parsed = messageSchema.safeParse(body);
         if(!parsed.success) {
           return Response.json({error: 'Invalid message'}, {status: 400});
         } 
-        const mesage = mesagestore.set({workspaceId, userId: Id}, { ...parsed.data, createdAt: new Date() });
-       console.log(mesagestore.size);
+       
+      const mesage =  await client.set(`lastMessage:${workspaceId}:${Id}`, JSON.stringify({ ...parsed.data, createdAt: new Date() }));
+        await client.quit();
+       
         return Response.json(mesage)
+        } catch (error) {
+          return Response.json({error: 'Internal Server Error',err:error}, {status: 500});
+        }
       },
     },
   },
